a26ea19de96dcfad384cdb0d9c3a71fd992768d1
Nakrani <ashishnakrani606@gmail.com>
2026-06-08 10:18:06 +0530

    Add swiper dependency

--- components/sections/Pricing.tsx
+++ components/sections/Pricing.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import Image from 'next/image';
 import Star from '../../public/images/star-orange.png';
 import Studio from '../../public/images/studio-01.jpg';
 import { ScrollAnimate } from '../ScrollAnimate';
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 export const Pricing = () => {
   return (
--- components/sections/ReelsDisplay.tsx
+++ components/sections/ReelsDisplay.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
